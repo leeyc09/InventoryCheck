b6880cebcce1f1c6728ef60186ff49f48d1a0ab2
isse <sanchezclarisse@gmail.com>
2017-01-01 22:22:30 +0800

    feat: add date and time function

--- app/src/main/java/app/miji/com/inventorycheck/activity/DeliveryActivityFragment.java
+++ app/src/main/java/app/miji/com/inventorycheck/activity/DeliveryActivityFragment.java
@@ -1,10 +1,17 @@
 package app.miji.com.inventorycheck.activity;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
 
 import app.miji.com.inventorycheck.R;
 
@@ -19,6 +26,61 @@ public class DeliveryActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_delivery, container, false);
+        View view = inflater.inflate(R.layout.fragment_delivery, container, false);
+
+        ImageButton btnDate = (ImageButton) view.findViewById(R.id.btn_date);
+        ImageButton btnTime = (ImageButton) view.findViewById(R.id.btn_time);
+        final EditText txtDate = (EditText) view.findViewById(R.id.txt_date);
+        final EditText txtTime = (EditText) view.findViewById(R.id.txt_time);
+
+        //when btnDate is clicked, show DatePickerDiaolog
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                final int mYear, mMonth, mDay;
+
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                //launch datepicker dialog initially picking current date
+                DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        String pickedDate = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                        txtDate.setText(pickedDate);
+                    }
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show(getActivity().getFragmentManager(), "Datepickerdialog");
+
+            }
+        });
+
+        //when btnTime is clicked,show TimePickerDialog
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                final int mHour, mMinute;
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                //launch datepicker dialog initially picking current time
+                TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+                        String pickedTime = hourOfDay + ":" + minute;
+                        txtTime.setText(pickedTime);
+                    }
+                },mHour,mMinute,false);
+                timePickerDialog.show(getActivity().getFragmentManager(), "Timepickerdialog");
+            }
+        });
+
+
+        return view;
     }
 }
