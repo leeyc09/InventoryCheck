2a6279581ea29f44b638fb5b6515a79ebf270d61
isse <sanchezclarisse@gmail.com>
2017-02-28 23:07:02 +0800

    feat: apply animation to crown logo in Main Activity

--- app/src/main/java/app/miji/com/inventorycheck/activity/MainActivity.java
+++ app/src/main/java/app/miji/com/inventorycheck/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package app.miji.com.inventorycheck.activity;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 import app.miji.com.inventorycheck.R;
 import app.miji.com.inventorycheck.adapter.MyHomeItemRecyclerViewAdapter;
@@ -38,7 +39,42 @@ public class MainActivity extends AppCompatActivity {
         assert recyclerView != null;
         setupRecyclerView(recyclerView);
 
+        startLogoAnimation();
 
+    }
+
+    private void startLogoAnimation() {
+        final ImageView mImageLogo = (ImageView) findViewById(R.id.image_logo);
+
+
+        final long DEFAULT_ANIMATION_DURATION = 1750L;
+
+        int appBarHeight = (int) getResources().getDimension(R.dimen.app_bar_height);
+
+
+        //Create an instance of ValueAnimator by calling the static method ofFloat.
+        // In this case, the values start at 0 and end with appBarHeight.
+        // Android starts screen coordinates at the top-left corner,
+        // it moves  top to bottom.
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, appBarHeight);
+
+        //ValueAnimator calls this listener with every update to the animated value
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //Get the current value from the animator and cast it to float;
+                // current value type is float because you created the ValueAnimator with ofFloat.
+                float value = (float) animation.getAnimatedValue();
+                //Change the image's position by using the setTranslationY().
+                mImageLogo.setTranslationY(value / 3);
+            }
+        });
+
+        //Set up the animator’s duration and interpolator.
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+        //start animation
+        valueAnimator.start();
     }
 
     private void setupRecyclerView(RecyclerView recyclerView) {
