32a9cc6763e293d0b4a36b230ae7b3b211a00053
isse <sanchezclarisse@gmail.com>
2017-04-23 23:52:52 +0800

    feat: add bottom sheet for stock take

--- app/src/main/java/app/miji/com/inventorycheck/fragment/StockTakeFragment.java
+++ app/src/main/java/app/miji/com/inventorycheck/fragment/StockTakeFragment.java
@@ -1,16 +1,21 @@
 package app.miji.com.inventorycheck.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.commons.MenuSheetView;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import app.miji.com.inventorycheck.R;
@@ -44,11 +49,54 @@ public class StockTakeFragment extends Fragment {
         }
 
         setupRecyclerView(recyclerView);
+
+        //bottom sheet
+        final BottomSheetLayout bottomSheet = (BottomSheetLayout) view.findViewById(R.id.bottomsheet);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setupBottomSheet(bottomSheet);
+            }
+        });
+
         return view;
+
     }
 
     private void setupRecyclerView(RecyclerView recyclerView) {
         mAdapter = new StockTakeRecyclerViewAdapter();
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void setupBottomSheet(final BottomSheetLayout bottomSheet){
+
+        MenuSheetView menuSheetView = new MenuSheetView(getActivity(), MenuSheetView.MenuType.LIST, "Create Stock Take", new MenuSheetView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getActivity(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                if (bottomSheet.isSheetShowing()) {
+                    bottomSheet.dismissSheet();
+                }
+
+                int id = item.getItemId();
+
+                if (id == R.id.action_manual) {
+                    //TODO: go to another activity or show a dialog box
+                }
+                if (id == R.id.action_scan) {
+                    //TODO: go to another implicit activity for scanning
+                }
+                if (id == R.id.action_bluetooth_scan) {
+                    //TODO: go to bluetooth scanning
+                }
+
+                return true;
+            }
+        });
+        menuSheetView.inflateMenu(R.menu.menu_scan);
+        bottomSheet.showWithSheetView(menuSheetView);
+
     }
 }
